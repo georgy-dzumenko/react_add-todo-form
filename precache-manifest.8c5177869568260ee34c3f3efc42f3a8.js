487c8780e25b8043267b222e279e117eb504d799
Georgy Dzumenko <erterj05@gmail.com>
2021-04-11 19:55:33 +0300

    Updates

---
+++ precache-manifest.8c5177869568260ee34c3f3efc42f3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320eebc09a21514883f715d1a4e86019",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ff73c89473d3cc567c2",
+    "url": "./static/css/main.993253bc.chunk.css"
+  },
+  {
+    "revision": "acefde52036cff9f81c8",
+    "url": "./static/js/2.677667ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.677667ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff73c89473d3cc567c2",
+    "url": "./static/js/main.a0ac308a.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
